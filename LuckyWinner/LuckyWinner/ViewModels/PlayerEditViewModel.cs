f0a78373b99bd9f3034d4c4fbeb10368820c39e2
stvansolano <stvansolano@gmail.com>
2015-11-28 08:26:45 -0600

    Added participants for Meetup group (November 2015)

--- LuckyWinner/LuckyWinner/ViewModels/PlayerEditViewModel.cs
+++ LuckyWinner/LuckyWinner/ViewModels/PlayerEditViewModel.cs
@@ -9,10 +9,63 @@
         public PlayerEditViewModel()
         {
             Title = "Sorteo";
-            Players = new ObservableCollection<PlayerViewModel>(new []
-            {
-                new PlayerViewModel { PlayerName = "Esteban Solano"}
+
+            Players = new ObservableCollection<PlayerViewModel>();
+
+            FillPlayers(new string [] {
+                "Carlos Argüello",
+                "Marco Antonio Gómez",
+                "Daniel Lacayo",
+                "Carlos Cervantes Gutierrez",
+                "Geraldin Torres",
+                "Xiannie Hylton",
+                "May Valverde",
+                "Richard Fernández",
+                "Herber Madrigal",
+                "Oscar Arroyo Fuentes",
+                "Aaron Cedeño Martí­nez",
+                "Andrés Carvajal",
+
+                "Andrey Alfaro",
+                "JC",
+                "Alejandra Herrera P",
+                "Cesar Guillen Oreamuno",
+                "Jairo Esquivel",
+                "Stuart Sanchez",
+                "Kenneth P. Barquero",
+                "Kimberly",
+                "Esteban (estsantre96@hotmail.com)",
+
+                "Deibyd",
+                "Esteban Hernandez",
+                "Javier Nuñez",
+                "Jose Alberto Gonzalez Sanchez",
+                "Keneth Murillo",
+                "Pablo Duran",
+                "Pablo Salas",
+                "Roberto Chacón",
+                "Steven",
+                "Thomas Edward Lee",
+                "Sergio Rios",
+                "Yoselin Vega",
+                "William Porras Quiros",
+                "Alexander Remedios",
+                "Alonso Blanco",
+                "Bayron Chavarria",
+                "Rosita",
+                "Laura Rojas",
+                "Walter",
             });
+        }
+
+        private void FillPlayers(string[] items)
+        {
+            Players.Clear();
+
+            foreach (var player in items)
+            {
+                Players.Add(new PlayerViewModel {PlayerName = player});
+            }
         }
 
         public ICollection<PlayerViewModel> Players { get; set; }
