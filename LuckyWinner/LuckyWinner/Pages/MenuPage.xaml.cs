6ab179037940065aa305b1c66822fc715cebc194
Esteban Solano G. <stvansolano@gmail.com>
2016-01-30 00:40:12 -0600

    Updated to Xamarin Forms 2.0 and renamed UI components

---
+++ LuckyWinner/LuckyWinner/Pages/MenuPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace LuckyWinner.Pages
+{
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using Xamarin.Forms;
+	using System.Linq;
+	using Views;
+
+	public partial class MenuPage
+	{
+		public MenuPage ()
+		{
+			InitializeComponent ();
+
+			var options = new ObservableCollection<View>();
+			options.Add(new GameView());
+
+			MenuOptions = options;
+
+			CurrentView = MenuOptions.First();
+		}
+
+		public IEnumerable<View> MenuOptions { get; set; }
+
+		public View CurrentView { get; set; }
+	}
+}
---
+++ LuckyWinner/LuckyWinner/Services/FeedService.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Shared
+{
+    using System.Threading.Tasks;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Newtonsoft.Json;
+    using System.Collections.ObjectModel;
+	using System.Diagnostics;
+
+    public class FeedService : RestService
+    {
+        private const string ApiAddress = "https://xamarin-workshop.firebaseio.com/";
+
+        public FeedService(NetworkService networkService) : base(ApiAddress, networkService)
+        {
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            var result = new List<Category>();
+
+            if (Network.IsConnected == false)
+            {
+                return result;
+            }
+				
+            try
+            {
+				var json = await Get("/Categories.json").ConfigureAwait(false);
+
+				// JSON.Net deserialization
+				var parsed = JsonConvert.DeserializeObject<Dictionary<object, Category>>(json);
+
+				return parsed.Values.ToArray();
+            }
+            catch (Exception ex)
+            {
+				Debug.WriteLine(ex.Message);
+            }
+            
+            return result;
+        }
+
+		public async Task<bool> Post(Category instance)
+		{
+			if (Network.IsConnected == false)
+			{
+				return false;
+			}
+				
+			try
+			{
+				var result = await Post("/Categories.json", instance).ConfigureAwait(false);
+
+				return result != null && result.StatusCode == HttpStatusCode.OK;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+
+			return false;
+		}
+
+
+    }
+}
